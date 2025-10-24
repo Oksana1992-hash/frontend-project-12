06736b507262e76bb9e5b627f379896b99d4d713
Oksana1992-hash <nox23061992@gmail.com>
2025-10-25 01:06:13 +0300

    Submitting a form with user data to the server has been implemented

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,39 +1,69 @@
 import { Formik, Form, Field } from 'formik'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import routes from '../routes'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { loginSuccess } from '../slices/authSlice'
+
 
 const LoginForm = () => {
-  <Formik
-    initialValues={{ username: '', password: '' }}
-    onSubmit={(values) => {
-      console.log('Форма отправлена', values)
-    }}
-  >
-    {() => (
-      <Form>
-        <h1>Войти</h1>
-        {/* Поле для имени пользователя */}
-        <div>
-          <label htmlFor="username">Имя пользователя</label>
-          <Field
-            id="username"
-            type="text"
-            name="username"
-            placeholder="Введите имя пользователя"
-          />
-        </div>
-        {/* Поле для пароля */}
-        <div>
-          <label htmlFor="password">Пароль</label>
-          <Field
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Введите пароль"
-          />
-        </div>
-        <button type="submit">Войти</button>
-      </Form>
-    )}
-  </Formik>
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [authError, setAuthError] = useState(false)
+
+  return (
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={async (values, { setSubmitting }) => {
+        setSubmitting(true)
+
+        try {
+          const responce = await axios.post(routes.loginPath(), values)
+          const token = responce.data
+          dispatch(loginSuccess({ token }))
+          setAuthError(false)
+          navigate('/')
+        }
+        catch (error) {
+          console.log(error.message)
+          setAuthError(true)
+        }
+
+        setSubmitting(false)
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <h1>Войти</h1>
+          {/* Поле для имени пользователя */}
+          <div>
+            <label htmlFor="username">Имя пользователя</label>
+            <Field
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Введите имя пользователя"
+              autoComplete="username"
+            />
+          </div>
+          {/* Поле для пароля */}
+          <div>
+            <label htmlFor="password">Пароль</label>
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Введите пароль"
+              autoComplete="current-password"
+            />
+          </div>
+          <div hidden={!authError}>Неверные имя пользователя или пароль</div>
+          <button type="submit" disabled={isSubmitting}>Войти</button>
+        </Form>
+      )}
+    </Formik>
+  )
 }
 
 export default LoginForm
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
-const HomePage = () => (
-  <div>
-    <h1>Главная страница</h1>
-  </div>
-)
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const HomePage = () => {
+  const navigate = useNavigate()
+
+  const token = useSelector((state) => state.auth.token)
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [])
+
+  return (
+    <div>
+      <h1>Главная страница</h1>
+    </div>
+  )
+}
 
 export default HomePage
