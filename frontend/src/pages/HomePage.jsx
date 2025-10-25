da2ea05fccc22928558571277b7a335e39f3ef8f
Oksana1992-hash <nox23061992@gmail.com>
2025-10-26 00:10:00 +0300

    Implemented receiving data from the server

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,17 +2,31 @@ import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { Container } from 'react-bootstrap'
+import axios from 'axios'
+import routes from '../routes'
 
 const HomePage = () => {
   const navigate = useNavigate()
 
-  const token = useSelector((state) => state.auth.token)
+  const userId = useSelector((state) => state.auth.token)
+  const token = userId?.token
 
   useEffect(() => {
-    if (!token) {
+    if (!userId) {
       navigate('/login')
+      return
     }
-  }, [])
+    const fetchData = async () => {
+      try {
+        const responce = await axios.get(routes.getChannelsPath(), { headers: { Authorization: `Bearer ${token}`, } })
+        console.log('responce Главная страница', responce.data)
+      }
+      catch (error) {
+        console.log(error.message)
+      }
+    }
+    fetchData()
+  }, [navigate, token, userId])
 
   return (
     <Container className="mt-5">
